96a65bf6535357b276c10c3423aba47e0eac1443
John Yeager <contatojhonatasc@gmail.com>
2023-04-21 14:35:31 -0300

    separando as classes por arquivos de Materias e Medias

---
+++ Mod8/br.com.exercicio/src/Materias.java
@@ -0,0 +1,19 @@
+
+public class Materias {
+
+    public static class Portugues {
+        public double portTrim1 = 2.5;
+        public double portTrim2 = 8.9;
+        public double portTrim3 = 6.0;
+        public double portTrim4 =8.0;
+        public double portSoma = portTrim1 + portTrim2 + portTrim3 + portTrim4;
+    }
+
+    public static class Matematica {
+        public double matTrim1 = 5.0;
+        public double matTrim2 = 6.1;
+        public double matTrim3 = 7.0;
+        public double matTrim4 = 4.0;
+        public double matSoma = matTrim1 + matTrim2 + matTrim3 + matTrim4;
+    }
+}
--- Mod8/br.com.exercicio/src/Medias.java
+++ Mod8/br.com.exercicio/src/Medias.java
@@ -4,8 +4,8 @@ public class Medias {
 
     public static void main(String args[]) {
         // instanciando as classes
-        Portugues port = new Portugues();
-        Matematica mat = new Matematica();
+        Materias.Portugues port = new Materias.Portugues();
+        Materias.Matematica mat = new Materias.Matematica();
         System.out.println("*** BOLETIM ***\n\nPORTUGUÊS:\n1º Trimestre: " +
                 port.portTrim1 + "\n2º Trimestre: " + port.portTrim2 + "\n3º Trimestre: " +
                 port.portTrim3 + "\n4º Trimestre: " + port.portTrim4 + "\n---- Média: " +
@@ -17,20 +17,4 @@ public class Medias {
 
     }
 
-
-    public static class Portugues {
-        public double portTrim1 = 2.5;
-        public double portTrim2 = 8.9;
-        public double portTrim3 = 6.0;
-        public double portTrim4 =8.0;
-        public double portSoma = portTrim1 + portTrim2 + portTrim3 + portTrim4;
-    }
-
-    public static class Matematica {
-        public double matTrim1 = 5.0;
-        public double matTrim2 = 6.1;
-        public double matTrim3 = 7.0;
-        public double matTrim4 = 4.0;
-        public double matSoma = matTrim1 + matTrim2 + matTrim3 + matTrim4;
-    }
 }
