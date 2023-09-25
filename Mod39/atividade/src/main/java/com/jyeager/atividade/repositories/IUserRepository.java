8dfb75f06bb239cfdd9c6355fa5273ca02a51c21
John Yeager <contatojhonatasc@gmail.com>
2023-09-25 13:35:10 -0300

    feat create user Entity via IntelliJ

---
+++ Mod39/atividade/src/main/java/com/jyeager/atividade/repositories/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.jyeager.atividade.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.jyeager.atividade.domain.user.User;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(String id);
+
+}
