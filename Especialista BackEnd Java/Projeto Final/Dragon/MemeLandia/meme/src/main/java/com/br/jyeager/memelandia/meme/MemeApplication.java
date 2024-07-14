3fa654f860c409fcb3ebb101d19feb14cdfff885
John Yeager <contatojhonatasc@gmail.com>
2024-07-14 06:05:55 -0300

    adicao de tarefa faltante

--- Especialista BackEnd Java/Projeto Final/Dragon/MemeLandia/meme/src/main/java/com/br/jyeager/memelandia/meme/MemeApplication.java
+++ Especialista BackEnd Java/Projeto Final/Dragon/MemeLandia/meme/src/main/java/com/br/jyeager/memelandia/meme/MemeApplication.java
@@ -2,8 +2,18 @@ package com.br.jyeager.memelandia.meme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+
+		"com.br.jyeager.memelandia.meme.entity",
+
+		"com.br.jyeager.memelandia.categoriameme.entity",
+
+		"com.br.jyeager.memelandia.usuario.entity"
+
+})
 public class MemeApplication {
 
 	public static void main(String[] args) {
